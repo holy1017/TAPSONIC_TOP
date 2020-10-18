ce99734fe79f8e0b2e7e9baef60885edb4af120c
holy <holy1017@naver.com>
2020-10-18 22:30:25 +0900

    1

--- TAPSONIC_TOP/Program.cs
+++ TAPSONIC_TOP/Program.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,11 +25,27 @@ namespace TAPSONIC_TOP
         {            
         }
 
-        enum Att
+        enum Att    // 속성
         {
             dancer,
             session,
             vocal
+        }
+
+        enum Type    // 타입
+        {
+            t,
+            l,
+            s,
+            p,
+            n
+        }
+
+        enum Buff
+        {
+            combo,
+            buff,
+            etc
         }
 
         // 캐릭별 off on 시간 (초)
@@ -36,73 +53,113 @@ namespace TAPSONIC_TOP
         {
             public string name;
             public Att att;
+            public Type type;
+            public Buff buff;
             public int off;
             public int on;
 
-            public Chra(string name, Att att, int off, int on)
+            public Chra(string name, Att att, Type type, Buff buff, int off, int on)
             {
                 this.name = name;
+                this.type = type;
+                this.buff = buff;
                 this.att = att;
-                this.off = off*mt;
-                this.on = on*mt;                
+                this.off = off * mt;
+                this.on = on * mt;
             }
 
-            public override string ToString() => $"({name}, {off}, {on})";
+            public override string ToString() => $"{name},\t {off},\t {on}";
         }
 
 
         static void Main(string[] args)
         {
-
-
             // 출력파일명 설정
             StringBuilder sb = new StringBuilder();
             sb.Append(@"TAPSONIC_TOP_");
             sb.Append(DateTime.Now.ToString("yyyyMMddHHmmss"));
             sb.Append(@".png");
-            Console.WriteLine(sb.ToString());
+            Console.WriteLine("출력파일:" + sb.ToString());
 
-            // 색상 설정
-            Color con= Color.FromArgb(255, 255, 0);     
-            Color coff= Color.FromArgb(255, 255, 0);
-            Color cdancer = Color.FromArgb(255, 0, 0);
-            Color csession = Color.FromArgb(0, 255, 0);
-            Color cvocal = Color.FromArgb(0, 0, 255);
-
-            // 캐릭 목록
+            List<Chra> cd2 = new List<Chra>();
             List<Chra> cd = new List<Chra>();
-            cd.Add(new Chra("엘리10", Att.vocal, 18, 14));
-            cd.Add(new Chra("제시10", Att.vocal, 12, 13));
-            cd.Add(new Chra("엘클리어10", Att.vocal, 18, 10));
-            cd.Add(new Chra("아리아10", Att.vocal, 16, 10));
-            cd.Add(new Chra("니콜10", Att.vocal, 15, 13));
-            cd.Add(new Chra("걸윙10", Att.vocal, 14, 10));
-            cd.Add(new Chra("셜리10", Att.dancer, 17, 10));
-            cd.Add(new Chra("라파엘10", Att.dancer, 16, 18));
-            cd.Add(new Chra("크라켄10", Att.dancer, 16, 10));
-            cd.Add(new Chra("호련10", Att.dancer, 15, 15));
-            cd.Add(new Chra("벨10", Att.dancer, 21, 10));
-            cd.Add(new Chra("루시퍼10", Att.session, 15, 14));
-            cd.Add(new Chra("트래셔10", Att.session, 19, 15));
-            cd.Add(new Chra("재규어10", Att.session, 15, 11));
-            cd.Add(new Chra("업튼10", Att.session, 18, 14));
-            cd.Add(new Chra("틴체르니10", Att.session, 17, 10));
-            cd.Add(new Chra("볼프강10", Att.session, 10, 9));
+
+            // 캐릭 세팅            
+            cd2.Add(new Chra("크라켄10", Att.dancer, Type.n, Buff.combo, 16, 10));
+            cd2.Add(new Chra("셜리10", Att.dancer, Type.n, Buff.combo, 17, 10));
+            cd2.Add(new Chra("호련10", Att.dancer, Type.n, Buff.combo, 15, 15));
+            cd2.Add(new Chra("벨10", Att.dancer, Type.n, Buff.combo, 21, 10));
+            cd2.Add(new Chra("라파엘10", Att.dancer, Type.n, Buff.combo, 16, 18));
+            cd2.Add(new Chra("볼프강10", Att.session, Type.n, Buff.combo, 10, 9));
+            cd2.Add(new Chra("재규어10", Att.session, Type.n, Buff.combo, 15, 11));
+            cd2.Add(new Chra("틴체르니10", Att.session, Type.n, Buff.combo, 17, 10));
+            cd2.Add(new Chra("루시퍼10", Att.session, Type.n, Buff.combo, 15, 14));
+            cd2.Add(new Chra("업튼10", Att.session, Type.n, Buff.combo, 18, 14));
+            cd2.Add(new Chra("트래셔10", Att.session, Type.n, Buff.combo, 19, 15));
+            cd2.Add(new Chra("걸윙10", Att.vocal, Type.n, Buff.combo, 14, 10));
+            cd2.Add(new Chra("제시10", Att.vocal, Type.n, Buff.combo, 12, 13));
+            cd2.Add(new Chra("아리아10", Att.vocal, Type.n, Buff.combo, 16, 10));
+            cd2.Add(new Chra("엘클리어10", Att.vocal, Type.n, Buff.combo, 18, 10));
+            cd2.Add(new Chra("니콜10", Att.vocal, Type.n, Buff.combo, 15, 13));
+            cd2.Add(new Chra("엘리10", Att.vocal, Type.n, Buff.combo, 18, 14));
+            cd2.Add(new Chra("베아트리스10", Att.vocal, Type.l, Buff.buff, 11, 20));
+            cd2.Add(new Chra("다이나10", Att.vocal, Type.l, Buff.etc, 9, 1));
+
+
+            // 출력
+            printChars(cd2);
+
+            // 입력받기
+            int ct2 = 0;
+            do
+            {
+                Console.WriteLine($"비교할 {ct2}번째 캐릭 번호를 누르세요. 4개 필요.");
+                Console.WriteLine($"x 입력시 종료. a 입력시 캐릭목록 다시보기. ");
+                printChars(cd);
+                Console.WriteLine("-------입력된 캐릭--------");                
+
+                string t = Console.ReadLine();
+                if (t == "x") return;
+                if (t == "a")
+                {
+                    printChars(cd2);
+                    continue;
+                }
+                if (!int.TryParse(t,out int c))
+                {
+                    Console.WriteLine($"숫자 아님");
+                    continue;
+                }
+                if (cd2.Count <= c || c < 0)
+                {
+                    Console.WriteLine($"유효한 범위 아님");
+                    continue;
+                }
+                cd.Add(cd2[c]);
+                ct2++;
+            } while (ct2 < 4);
+
+            // 테스트용
+            // cd2.ForEach(delegate (Chra c)
+            // {
+            //     cd.Add(c);
+            // });
 
             // 1. Create a bitmap
-            using (Bitmap bitmap = new Bitmap(tw+sw, ch * cd.Count, PixelFormat.Format32bppArgb))
+            using (Bitmap bitmap = new Bitmap(tw + sw, ch * cd.Count, PixelFormat.Format32bppArgb))
             {
                 var graphics = Graphics.FromImage(bitmap);
                 var fontb = new Font("맑은 고딕", 16, FontStyle.Bold, GraphicsUnit.Pixel);
                 var fontf = new Font("맑은 고딕", 16, FontStyle.Regular, GraphicsUnit.Pixel);
-                int cnt=0;
-                Brush b= Brushes.Black;
+                int cnt = 0;
+                Brush b = Brushes.Black;
+                Brush b2 = Brushes.Black;
 
                 // 기본 영역
                 graphics.FillRectangle(Brushes.Black,
                     0,
                     0,
-                    tw + sw , ch * cd.Count); //line
+                    tw + sw, ch * cd.Count); //line
 
                 // 세로 경계선
                 graphics.FillRectangle(Brushes.White,
@@ -111,7 +168,8 @@ namespace TAPSONIC_TOP
                     ch2, ch * cd.Count); //line
 
                 // 캐릭마다 색 설정
-                cd.ForEach(delegate (Chra c) {
+                cd.ForEach(delegate (Chra c)
+                {
                     switch (c.att)
                     {
                         case Att.dancer:
@@ -127,15 +185,28 @@ namespace TAPSONIC_TOP
                             b = Brushes.Black;
                             break;
                     }
-
+                    switch (c.buff)
+                    {
+                        case Buff.combo:
+                            b2= Brushes.Yellow;
+                            break;
+                        case Buff.buff:
+                            b2 = Brushes.DarkGreen;
+                            break;
+                        case Buff.etc:
+                            b2 = Brushes.DarkRed;
+                            break;
+                        default:
+                            break;
+                    }
                     // 그리기 반복
-                    for (int i = tw; i < sw+tw; i+=c.off+c.on)
-                    {                    
+                    for (int i = tw; i < sw + tw; i += c.off + c.on)
+                    {
                         //graphics.FillRectangle(Brushes.Black,
                         //    i,
                         //    20*cnt,
                         //    c.off, ch1); //off
-                        graphics.FillRectangle(Brushes.Yellow,
+                        graphics.FillRectangle(b2,
                             i + c.off,
                             20 * cnt,
                             c.on, ch1); //on
@@ -174,8 +245,18 @@ namespace TAPSONIC_TOP
             }
 
 
+            Console.WriteLine("\n앤터시 종료");
+            Console.ReadLine();
+        }
 
-            //Console.ReadLine();
+        private static void printChars(List<Chra> cd2)
+        {
+            int ct = 0;
+            cd2.ForEach(delegate (Chra c)
+            {
+                Console.Write(ct++ + ":");
+                Console.WriteLine(c.ToString());                
+            });
         }
     }
 }
